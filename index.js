81fdf74eec1c5827639037dfd44e74ebc245d2d3
Alice Russell <alice.louise.russell@gmail.com>
2019-11-21 10:39:05 -0500

    color choice prompt

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ const inquirer = require("inquirer");
 
 
 inquirer
-    .prompt({
+    .prompt([{
+        type: "input",
         name: "username",
         message: "What is your GitHub username?"
-    })
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["green", "blue", "pink", "red"]
+    }])
     .then(function({ username }) {
         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
